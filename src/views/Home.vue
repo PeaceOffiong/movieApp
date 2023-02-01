677595ad57caf8664888fe57f16baca36b705ef9
peaceOffiong <peaceyben@gamil.com>
2023-02-01 08:29:17 -0800

    Home page improvements made on UI

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <div class="feature-card">
+            <router-link to="/movie/tt0409591">
+
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
